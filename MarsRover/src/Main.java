ee43a3fddac5ff29ec6ece19169db8cd5cc1307a
Lucía Parga <luci.parga@hotmail.com>
2020-01-29 18:52:50 +0000

    Update Main.java

--- MarsRover/src/Main.java
+++ MarsRover/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Interpreter MarsRover = new Interpreter();
-		MarsRover.readFile("/Users/lusy/Desktop/MarsroverJavaLP/MarsRover//src/marsRover.txt");
+		MarsRover.readFile("src/marsRover.txt");
 	}
 }
